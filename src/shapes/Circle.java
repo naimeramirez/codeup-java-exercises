54aedf7189ff6b64ccf0d58eae762e1d54389e1f
naimeramirez <127249348+naimeramirez@users.noreply.github.com>
2023-05-22 16:41:09 -0600

    working on circle exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,19 @@
 package shapes;
 
 public class Circle {
+
+    private double radius;
+
+    public Circle (double radius) {
+
+    }
+
+    public double getArea () {
+
+    }
+
+    public double getCircumference () {
+
+    }
+
 }
